a6749827d62c4d96243cdb152bdb822acb6792e1
blanc091 <mircea.albu91@gmail.com>
2025-02-23 04:07:24 +0200

    Implemented ASPIdentity.

---
+++ Migrations/Models/Case.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+
+public class Case
+{
+    public int CaseId { get; set; }
+    public required string CaseName { get; set; }
+    public int UserId { get; set; }
+    public required bool IsOngoing { get; set; }
+    public required bool IsWon { get; set; }
+    public ICollection<Document>? Documents { get; set; }
+    public required User User { get; set; }
+}
+
---
+++ Migrations/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+public class User : IdentityUser
+{
+    public override required string Id { get; set; }
+    public required string Username { get; set; }
+    public override required string PasswordHash { get; set; }
+    public override required string Email { get; set; }
+    public required string PreferredName { get; set; }
+    public bool IsActive { get; set; }
+    public bool HasActiveCase { get; set; }
+    public ICollection<Case>? Cases { get; set; }
+}
